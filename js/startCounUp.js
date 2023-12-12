6bcdb0109ead92b6c36c7d418a41a67e16c18b6d
shuyu36 <sharon87306@gmail.com>
2023-12-12 16:15:53 +0800

    1212首頁

--- js/startCounUp.js
+++ js/startCounUp.js
@@ -1,24 +1,25 @@
 // 保存為 demo.js
 import { CountUp } from "../node_modules/countup.js/dist/countUp.min.js";
 document.addEventListener("DOMContentLoaded", function () {
-//   window.addEventListener("load", () => {
-    const options = {
-      decimalPlaces: 0, // 不要小數點
-      duration: 1.5,
-      useGrouping: true, // 啟用千分位分隔符
-      enableScrollSpy: true,
-    };
+  const options = {
+    startVal: 10000,
+    decimalPlaces: 0, // 不要小數點
+    duration: 4,
+    useGrouping: true, // 啟用千分位分隔符
+    enableScrollSpy: true,
+  };
 
-    const options2 = {
-      decimalPlaces: 1, // 不要小數點
-      duration: 2,
-      useGrouping: false, // 啟用千分位分隔符
-      enableScrollSpy: true,
-    };
+  const options2 = {
+    startVal: 11.1,
+    decimalPlaces: 1, // 不要小數點
+    duration: 3,
+    useGrouping: false, // 啟用千分位分隔符
+    enableScrollSpy: true,
+  };
 
-    let demo1 = new CountUp("num1", 40013, options);
-    let demo2 = new CountUp("num2", 89.7, options2);
-    let demo3 = new CountUp("num3", 96.3, options2);
-    let demo4 = new CountUp("num4", 78.6, options2);
-//   });
+  let demo1 = new CountUp("num1", 40013, options);
+  let demo2 = new CountUp("num2", 89.7, options2);
+  let demo3 = new CountUp("num3", 96.3, options2);
+  let demo4 = new CountUp("num4", 78.6, options2);
+
 });
